3c048633c60c09956330ab8175496c86951cfdc9
fagathe-dev <agathefrederick@gmail.com>
2026-09-16 00:25:28 +0200

    chore: fix api CORS configuration

--- todo-api/src/server.ts
+++ todo-api/src/server.ts
@@ -7,18 +7,17 @@ const app = express();
 const PORT = Number(process.env.PORT) || 5000;
 
 // Configuration CORS explicite pour la prod et le local
-const corsOptions = {
-  origin: [
-    'https://react-todo-app.frederickagathe.fr',
-    'http://localhost:5173',
-  ],
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.use(
+  cors({
+    origin: [
+      'https://react-todo-app.frederickagathe.fr',
+      'http://localhost:5173',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
