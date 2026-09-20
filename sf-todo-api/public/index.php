667769e4b6e4936c8db15440b369181be14d64f1
fagathe-dev <agathefrederick@gmail.com>
2026-09-20 08:26:24 +0200

    chore: fix /api/index.php

--- sf-todo-api/public/index.php
+++ sf-todo-api/public/index.php
@@ -2,7 +2,13 @@
 
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+// Réaligne la racine pour annuler le faux sous-dossier injecté par le lien symbolique Apache
+if (isset($_SERVER['SCRIPT_NAME']) && str_starts_with($_SERVER['SCRIPT_NAME'], '/api/')) {
+    $_SERVER['SCRIPT_NAME'] = '/index.php';
+    $_SERVER['PHP_SELF'] = '/index.php';
+}
+
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
 return static function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
